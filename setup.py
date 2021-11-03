689a6bd36047bedbbeb432e8558c05240352408d
GitHub Action <action@github.com>
2021-11-03 15:36:22 +0000

    Automatic sync

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "GmodStore SDK"
-VERSION = "1.4.2"
+VERSION = "1.4.3"
 # To install the library, run the following
 #
 # python setup.py install
